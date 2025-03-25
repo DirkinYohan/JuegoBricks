120939cb89e7a7a8931872fce4152942398bca60
Dirkin Yohan Ojeda <dirkin.ojeda@campusucc.edu.co>
2025-03-25 20:06:02 +0000

    Ingresando nuevos archivos a src y tests

--- tests/test_circulo.py
+++ tests/test_circulo.py
@@ -1,5 +1,17 @@
-import pytest
-from src.circulo import *  # Importa el módulo desde el paquete src
+import unittest
+import math
+from src.circulo import calcular_area_circulo, calcular_perimetro_circulo
 
-def test_placeholder():
-    assert True  # TODO: Reemplazar con pruebas reales
+class TestCirculo(unittest.TestCase):
+    def test_calcular_area_circulo(self):
+        self.assertAlmostEqual(calcular_area_circulo(1), math.pi, places=5)
+        self.assertAlmostEqual(calcular_area_circulo(0), 0, places=5)
+        self.assertAlmostEqual(calcular_area_circulo(2), 4 * math.pi, places=5)
+
+    def test_calcular_perimetro_circulo(self):
+        self.assertAlmostEqual(calcular_perimetro_circulo(1), 2 * math.pi, places=5)
+        self.assertAlmostEqual(calcular_perimetro_circulo(0), 0, places=5)
+        self.assertAlmostEqual(calcular_perimetro_circulo(2), 4 * math.pi, places=5)
+
+if __name__ == '__main__':
+    unittest.main()
