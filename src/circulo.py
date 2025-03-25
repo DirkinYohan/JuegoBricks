84b23a5ef22abbb83ace37b6a6a3bc4706d8cbfb
Dirkin Yohan Ojeda <dirkin.ojeda@campusucc.edu.co>
2025-03-25 20:02:10 +0000

    Ingresando nuevos archivos a src y tests

---
+++ src/circulo.py
@@ -0,0 +1,13 @@
+import math
+
+def calcular_area_circulo(radio):
+    return math.pi * radio ** 2
+
+def calcular_perimetro_circulo(radio):
+    return 2 * math.pi * radio
+
+# Ejemplo de uso
+radio = 5
+print(f"Para un círculo de radio {radio}:")
+print(f"Área: {calcular_area_circulo(radio):.2f}")
+print(f"Perímetro: {calcular_perimetro_circulo(radio):.2f}")
