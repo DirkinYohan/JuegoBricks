58b3cc65dbbce8d7986113d939ca161f8355b1e9
Dirkin Yohan Ojeda <dirkin.ojeda@campusucc.edu.co>
2025-03-25 20:42:09 +0000

    Ingresando nuevos archivos a src y tests

--- tests/test_palabra.py
+++ tests/test_palabra.py
@@ -4,7 +4,7 @@ from src.palabra import contar_palabras_y_letras  # Importa la función desde tu
 class TestContarPalabrasYLetras(unittest.TestCase):
     def test_contar_palabras_y_letras(self):
         self.assertEqual(contar_palabras_y_letras("Hola mundo"), (2, 9))
-        self.assertEqual(contar_palabras_y_letras("Python es genial"), (3, 13))
+        self.assertEqual(contar_palabras_y_letras("Python es genial"), (3, 14))
         self.assertEqual(contar_palabras_y_letras(""), (0, 0))
         self.assertEqual(contar_palabras_y_letras("123 4567"), (2, 7))
         self.assertEqual(contar_palabras_y_letras("Un solo"), (2, 6))
